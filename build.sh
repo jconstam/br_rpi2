421ec6c27961825281a8ea4d36fd9f2780189a06
Jason <callmebob@gmail.com>
2021-03-26 15:32:07 -0600

    Fixed clean in build script.  Moved to stable version of binutils, increased size of rootfs.

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/bash
 
+git -C buildroot/ clean -fdx
 cp config_raspberrypi2 buildroot/.config
-git clean -fdx
 git -C buildroot/ checkout master
 git -C buildroot/ pull
 export FORCE_UNSAFE_CONFIGURE=1
